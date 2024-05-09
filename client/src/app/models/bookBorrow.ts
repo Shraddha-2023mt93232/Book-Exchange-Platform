83a14c8eaaf3fc74b13e4350a805e1ffecce3f98
Mehak Talwar <mehak.talwar@pb.com>
2024-05-10 01:54:52 +0530

    added book borrow and delete account

---
+++ client/src/app/models/bookBorrow.ts
@@ -0,0 +1,15 @@
+export class bookBorrow{
+    constructor(){
+      this.borrowerName = "";
+      this.bookName = "";
+      this.description = "";
+      this.dateOfIssue = "";
+      this.noOfIssuedDays = 0;
+    }
+    borrowerName: string;
+    bookName: string;
+    description: string;
+    dateOfIssue: string;
+    noOfIssuedDays: number
+  
+  }
--- client/src/app/pages/layout/layout.component.ts
+++ client/src/app/pages/layout/layout.component.ts
@@ -2,19 +2,28 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Table } from 'primeng/table';
 import { ViewChild } from '@angular/core';
+import { bookBorrow } from '../../models/bookBorrow';
+import { EventEmitter, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
+
+
+
 export class LayoutComponent {
-  showModalPopup = false
+  
+  showModalPopup = false;
+  currBookName: string = "";
+  borrowBookData: any;
   tableHeaders = [
     "Book Title",
     "Author Name",
     "Released On",
-    "Available", 
+    "Borrow", 
     "Actions"
   ]
   user: any;
@@ -38,24 +47,70 @@ export class LayoutComponent {
       available: true
     }
   ]
+  showDelete = false
+  borrowerDetails: any;
   constructor(private router: Router) {
     const localUser = localStorage.getItem('loggedUser');
     if(localUser != null) {
       this.user = JSON.parse(localUser);
     }
   }
+ 
+  borrower: any = {};
+
+  @Output() submitFormEvent = new EventEmitter<any>();
+  @Output() closeModalEvent = new EventEmitter<void>();
+
+  submitForm() {
+    this.showModalPopup = false
+    console.log(this.borrower)
+    //add api here to push data to table
+  }
+
   
+  openModal(book: string){
+    this.showModalPopup = true;
+    this.currBookName = book
+    
+
+  }
+  handleSubmitForm(borrower: any) {
+    console.log('Borrower details submitted:', borrower);
+    // You can handle the borrower details here, like updating your borrower list
+    this.closeModal();
+  }
+  closeModal() {
+    this.showModalPopup = false;
+    this.closeModalEvent.emit();
+  }
+  closeModalDelete(){
+    this.showModalPopup = true;
+  }
+  confirmDelete(){
+    // delete user api
+    console.log(this.user)
+  }
+  ngOninit (){
+    this.borrowBookData = new bookBorrow()
+  }
   onLogoff() {
     localStorage.removeItem('user');
     this.router.navigateByUrl('/login')
   }
+  
+
+  formModalDelete(){
+
+  }
 
   deleteAccount(user: string){
-    this.showModalPopup=true
-    console.log(user)
+    this.showDelete = true
+    this.user = user
     // ccmd to delete user
     // this.showModalPopup=false
   }
 }
+
+
 
 
